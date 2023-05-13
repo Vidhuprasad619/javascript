0f5cf47bee62e89a1d6fc0396cf97ac6c54066f9
Vidhuprasad619 <vidhuprasadcp619@gmail.com>
2023-05-13 12:21:59 +0530

    second commit

---
+++ Operators/comparison.js
@@ -0,0 +1,12 @@
+let a=10;
+let b=20;
+
+if(a===b){
+    console.log('a and b are same');
+}
+else if(a<b){
+    console.log('b is greater than a');
+}
+else{
+    console.log('a is greater than b');
+}
