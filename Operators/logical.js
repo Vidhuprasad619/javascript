a1e60661750809d91345bc512bd5553c989da62b
Vidhuprasad619 <vidhuprasadcp619@gmail.com>
2023-05-13 12:38:55 +0530

    logical operators

---
+++ Operators/logical.js
@@ -0,0 +1,16 @@
+let a=10;
+let b=20;
+
+if(a>5 && a<b){
+    console.log('condition of a success');
+}
+else{
+    console.log('condition of a failed');
+}
+
+if(b==a || b>15){
+    console.log('condition of b success');
+}
+else{
+    console.log('condition of b filed');
+}
