4600321c59796fba24776bccd44c17208aac99db
Vidhuprasad619 <vidhuprasadcp619@gmail.com>
2023-05-13 12:02:22 +0530

    first commit

---
+++ Operators/arithmetic.js
@@ -0,0 +1,11 @@
+let x=10;
+let y=5;
+
+console.log(x,'+',y,'=',x+y); //addition
+console.log(x,'-',y,'=',x-y); //subtraction
+console.log(x,'*',y,'=',x*y); //multiplication
+console.log(x,'/',y,'=',x/y); //division
+console.log(x,'**',y,'=',x**y); //exponentiation
+console.log(x,'%',y,'=',x%y); //modulus
+console.log('increment of x =',x++); //postincrement
+console.log('decrement of y =',--y); //predecrement
